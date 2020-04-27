ae4aa633192164650aac95bd88b1ae8f474342e5
Benjamin Staneck <staneck@gmail.com>
2020-04-27 17:18:04 +0200

    clean up unused rules

--- src/assets/javascripts/utilities/jsx/index.ts
+++ src/assets/javascripts/utilities/jsx/index.ts
@@ -110,6 +110,7 @@ export function h(
  * Namespace
  * ------------------------------------------------------------------------- */
 
+// eslint-disable-next-line no-redeclare
 export declare namespace h {
   namespace JSX {
     type Element = HTMLElement
